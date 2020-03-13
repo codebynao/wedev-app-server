af91de51b32c707ec58b8d0713da451105334c88
Naomi <naomi.paulmin@gmail.com>
2020-03-13 09:31:45 +0100

    :sparkles: add verification on CRUD

--- controllers/client.js
+++ controllers/client.js
@@ -5,10 +5,15 @@ const { creationSchema, updateSchema } = require('./../schemas/client');
 const Joi = require('@hapi/joi');
 
 class Client {
-  async createClient(args) {
+  async createClient(args, context) {
     try {
+      // Check client arguments validity
       Joi.assert(args.client, creationSchema);
 
+      // Check if logged in user is authorized to perform this action
+      if (!context.user || args.client.user !== context.user._id.toString()) {
+        throw new AuthenticationError('Action non autorisée');
+      }
       return await ClientModel.create(args.client);
     } catch (error) {
       console.error('Error createClient', error);
@@ -16,9 +21,15 @@ class Client {
     }
   }
 
-  async updateClient(args) {
+  async updateClient(args, context) {
     try {
+      // Check client arguments validity
       Joi.assert(args.client, updateSchema);
+
+      // Check if logged in user is authorized to perform this action
+      if (!context.user || hasClientPermission(context.user, args.client._id)) {
+        throw new AuthenticationError('Action non autorisée');
+      }
 
       return await ClientModel.findByIdAndUpdate(
         args.client._id,
@@ -31,8 +42,12 @@ class Client {
     }
   }
 
-  async deleteClient(args) {
+  async deleteClient(args, context) {
     try {
+      // Check if logged in user is authorized to perform this action
+      if (!context.user || hasClientPermission(context.user, args._id)) {
+        throw new AuthenticationError('Action non autorisée');
+      }
       await ClientModel.findByIdAndUpdate(args._id, {
         $set: { isDeleted: true }
       });
@@ -43,13 +58,37 @@ class Client {
     }
   }
 
-  async getClient(args) {
+  async getClient(args, context) {
     try {
+      // Check if logged in user is authorized to perform this action
+      if (!context.user || hasClientPermission(context.user, args._id)) {
+        throw new AuthenticationError('Action non autorisée');
+      }
       return await ClientModel.findById(args._id);
     } catch (error) {
       console.error('Error getClient', error);
       throw new Error(error.message || error);
     }
+  }
+
+  async getClients(context) {
+    try {
+      // Check if logged in user is authorized to perform this action
+      if (!context.user) {
+        throw new AuthenticationError('Action non autorisée');
+      }
+
+      return await ClientModel.find({ user: context.user._id });
+    } catch (error) {
+      console.error('Error getClients', error);
+      throw new Error(error.message || error);
+    }
+  }
+
+  hasClientPermission(contextUser, clientId) {
+    return (
+      contextUser.clients && contextUser.clients.find(id => id === clientId)
+    );
   }
 }
 
